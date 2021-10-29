e4a7a649e3587638273a5c69cb9f6a63e643fcbb
jared <ec2-user@ip-172-31-24-36.us-east-2.compute.internal>
2021-10-29 19:01:00 +0000

    updated ruby and rails version

--- web/CASTNXT/db/seeds.rb
+++ web/CASTNXT/db/seeds.rb
@@ -1,13 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-users = [{:first_name => 'Jane', :last_name => 'Doe', :agency_name => 'Test Agency', :address => '1234 test blvd.', :phone_number => '123-456-7890', :email_address => 'jane.doe@testmail.com', :height_feet => 5, :height_inches => 4, :dress_size => 5}]
-
-users.each do |user|
-    User.create!(user)
-end
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
